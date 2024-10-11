cb6854a18adb79b82c054adee593847a8bfa715e
Martin Anderson <mander24@students.cs.ubc.ca>
2024-10-11 10:29:57 -0700

    Specified the public methods required for starting the application and made the application start when run

---
+++ src/main/ui/TextFileApp.java
@@ -0,0 +1,10 @@
+package ui;
+
+// Represents the application that allows users to add .txt files from their computer to the program and sort and
+// browse through them
+public class TextFileApp {
+    // EFFECTS: starts the Text File application
+    public TextFileApp() {
+        
+    }
+}
