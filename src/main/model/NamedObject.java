9319785f5a439f640e08130fb003cfe6873aa31c
Martin Anderson <mander24@students.cs.ubc.ca>
2024-10-11 10:53:43 -0700

    Wrote tests for NamedObject

--- src/main/model/NamedObject.java
+++ src/main/model/NamedObject.java
@@ -1,7 +1,8 @@
 package model;
 
 public abstract class NamedObject {
-    // EFFECTS: returns true if input perfectly matches the beginning of this file's name otherwise false
+    // EFFECTS: returns true if input perfectly matches the beginning of this file's name including the empty string
+    //                  otherwise false
     public boolean isBeginningOfName(String input) {
         return false;
     }
--- src/test/model/TestNamedObject.java
+++ src/test/model/TestNamedObject.java
@@ -1,6 +1,6 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,7 +13,112 @@ public abstract class TestNamedObject {
     abstract void runBefore();
 
     @Test
-    void sampleTest() {
-        assertTrue(true);
+    void testIsBeginningOfNameEmptyInput() {
+        namedObject.setName("Apple");
+        assertTrue(namedObject.isBeginningOfName(""));
+    }
+
+    @Test
+    void testIsBeginningOfNameEmptyInputs() {
+        namedObject.setName("Orange");
+        assertTrue(namedObject.isBeginningOfName(""));
+        assertFalse(namedObject.isBeginningOfName("Apple"));
+    }
+
+    @Test
+    void testIsBeginningOfNameNoMatch() {
+        namedObject.setName("Apple");
+        assertFalse(namedObject.isBeginningOfName("Coconut"));
+    }
+
+    @Test
+    void testIsBeginningOfNameJustFirstCharacterMatches() {
+        namedObject.setName("Raspberry");
+        assertFalse(namedObject.isBeginningOfName("Rose Hip"));
+    }
+
+    @Test
+    void testIsBeginningOfNameFirstCharactersButNotAllMatch() {
+        namedObject.setName("Apple");
+        assertFalse(namedObject.isBeginningOfName("Apricot"));
+    }
+
+    @Test
+    void testIsBeginningOfNameOneCharacterAndMatches() {
+        namedObject.setName("Apple");
+        assertFalse(namedObject.isBeginningOfName("A"));
+    }
+
+    @Test
+    void testIsBeginningOfNameMultipleCharactersButNotFullAndMatches() {
+        namedObject.setName("Orange");
+        assertFalse(namedObject.isBeginningOfName("Ora"));
+    }
+
+    @Test
+    void testIsBeginningOfNameAllCharactersMatch() {
+        namedObject.setName("Apple");
+        assertTrue(namedObject.isBeginningOfName("Apple"));
+    }
+
+    @Test
+    void testIsBeginningOfNameAllCharactersMatchButInputIsLonger() {
+        namedObject.setName("Orange");
+        assertFalse(namedObject.isBeginningOfName("Oranges"));
+    }
+
+    @Test
+    void testIsBeginningOfNameWrongCase() {
+        namedObject.setName("Orange");
+        assertFalse(namedObject.isBeginningOfName("orange"));
+    }
+
+
+    @Test
+    void testIsNamedEmpty() {
+        namedObject.setName("Acer");
+        assertFalse(namedObject.isNamed(""));
+    }
+
+    @Test
+    void testIsNamedAllCharactersWrong() {
+        namedObject.setName("Acer");
+        assertFalse(namedObject.isNamed("MSI"));
+    }
+
+    @Test
+    void testIsNamedFirstCharacterWrong() {
+        namedObject.setName("Lenovo");
+        assertFalse(namedObject.isNamed("Penovo"));
+    }
+
+    @Test
+    void testIsNamedMiddleCharacterWrong() {
+        namedObject.setName("Lenovo");
+        assertFalse(namedObject.isNamed("Lenxvo"));
+    }
+
+    @Test
+    void testIsNamedLastCharacterWrong() {
+        namedObject.setName("ASUS");
+        assertFalse(namedObject.isNamed("ASUT"));
+    }
+
+    @Test
+    void testIsNamedRight() {
+        namedObject.setName("ASUS");
+        assertTrue(namedObject.isNamed("ASUS"));
+    }
+
+    @Test
+    void testIsNamedRightButLonger() {
+        namedObject.setName("HP");
+        assertFalse(namedObject.isNamed("HPs"));
+    }
+
+    @Test
+    void testIsNamedWrongCase() {
+        namedObject.setName("HP");
+        assertFalse(namedObject.isNamed("Hp"));
     }
 }
