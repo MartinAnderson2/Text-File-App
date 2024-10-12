53cdf2f20b0eaac15f03a5e1c586c1989ffa3a26
Martin Anderson <mander24@students.cs.ubc.ca>
2024-10-12 00:20:31 -0700

    Began the implementation of the application

--- src/main/ui/TextFileApp.java
+++ src/main/ui/TextFileApp.java
@@ -1,10 +1,47 @@
 package ui;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import model.*;
+
 // Represents the application that allows users to add .txt files from their computer to the program and sort and
-// browse through them
+// browse through them. Structure design based on "TellerApp"
 public class TextFileApp {
+    private Scanner scanner;
+    List<Folder> rootFolders;
+
     // EFFECTS: starts the Text File application
     public TextFileApp() {
-        
+        runTextFileApp();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void runTextFileApp() {
+        initialize();
+
+        welcome();
+
+        while (true) {
+            displayCommands();
+            String input = scanner.next().toLowerCase();
+        }
+    }
+    
+    // MODIFIES: this
+    // EFFECTS: initializes the application: instantiates rootFolders
+    private void initialize() {
+        rootFolders = new ArrayList<Folder>();
+    }
+
+    // EFFECTS: displays the first level of menu options that are available
+    private void displayCommands() {
+
+    }
+    
+    // EFFECTS: sends the user a welcome message
+    private void welcome() {
+        System.out.println("Welcome to the Text File application!");
     }
 }
